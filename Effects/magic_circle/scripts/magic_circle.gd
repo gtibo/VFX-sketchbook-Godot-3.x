517655e5e244c58b9e7e7ae5d836d8a672ce2664
Your Name <gtibo.contact@gmail.com>
2022-03-18 11:53:13 +0100

    Control update

--- Effects/magic_circle/scripts/magic_circle.gd
+++ Effects/magic_circle/scripts/magic_circle.gd
@@ -1,13 +1,21 @@
 extends Spatial
-
 export(PackedScene) var to_invoke
 
+var open = true
 
-func _on_Open_pressed():
+func _ready():
 	$AnimationPlayer.play("Appear")
 
-
+func _on_Open_pressed():
+	if open: return
+	$AnimationPlayer.play("Appear")
+	open = true
+func _on_Close_pressed():
+	if !open: return	
+	$AnimationPlayer.play_backwards("Appear")
+	open = false
 func _on_Invoke_pressed():
+	if !open : return
 	$AnimationPlayer.play("Invoke")
 	var b = to_invoke.instance()
 	b.translation = $Target.translation
